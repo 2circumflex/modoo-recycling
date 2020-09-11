253ab19ef56a51171d3190e131de5a3c960d8873
roy <2circumflex>
2020-09-11 19:43:12 +0900

    Fix eslint error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
+import '../styles/index.css'
 
 function Index() {
-  const [date, setDate] = useState(null);
+  const [date, setDate] = useState(null)
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+      const res = await fetch('/api/date')
+      const newDate = await res.text()
+      setDate(newDate)
     }
-    getDate();
-  }, []);
+    getDate()
+  }, [])
   return (
     <main>
       <Helmet>
@@ -50,7 +50,7 @@ function Index() {
       <h2>The date according to Node.js (TypeScript) is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
-  );
+  )
 }
 
-export default Index;
+export default Index
