a4bece4ea06bf138151ba9c3fecf961b11dba8c1
Sujin.Choi <amor77777777778@gmail.com>
2020-09-12 12:54:00 +0900

    Add How-to Page

---
+++ src/pages/how-to.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const HowTo: React.FC = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>How-to</title>
+      </Helmet>
+      <h1>How-to</h1>
+    </div>
+  )
+}
+
+export default HowTo
