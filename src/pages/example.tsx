5c3a2cbb8879729523383df275afd423cfa251b3
roy <2circumflex>
2020-09-15 17:41:07 +0900

    Add gatsby-plugin-intl

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useIntl, FormattedMessage } from 'gatsby-plugin-intl'
 
 const Example: React.FC = () => {
+  const intl = useIntl()
   return (
     <React.Fragment>
       라이브러리 사용법 예제
+      {/* https://www.gatsbyjs.com/plugins/gatsby-plugin-intl/ */}
+      <br />
+      {intl.formatMessage({ id: 'test' })}
+      <br />
+      {<FormattedMessage id='test' />}
     </React.Fragment>
   )
 }
