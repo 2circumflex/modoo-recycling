5e7046f693c05ef219ecf646f553863886b6d435
roy <2circumflex>
2020-09-16 17:31:39 +0900

    Add gatsby-image

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import { useIntl, FormattedMessage } from 'gatsby-plugin-intl'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 const Example: React.FC = () => {
   const intl = useIntl()
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: {eq: "recycle-test.jpg"}) {
+        childImageSharp {
+          fixed(height: 300, width: 300) {
+            base64
+            width
+            height
+            src
+            srcSet
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <React.Fragment>
       라이브러리 사용법 예제
@@ -11,6 +29,9 @@ const Example: React.FC = () => {
       {intl.formatMessage({ id: 'test' })}
       <br />
       {<FormattedMessage id='test' />}
+      <br />
+      {/* https://www.gatsbyjs.com/plugins/gatsby-image/ */}
+      <Img fixed={data.file.childImageSharp.fixed} />
     </React.Fragment>
   )
 }
